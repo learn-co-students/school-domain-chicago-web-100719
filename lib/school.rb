2485ec0387d768072075838e34046666894f5da2
kHuynhGitHub <huynh.h.kiimberly@gmail.com>
2019-10-09 09:13:12 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, add)
+        roster[add] ||= []
+        roster[add] << student
+    end
+
+    def grade(add)
+        roster.detect do | x, y|
+            if x == add
+                return y
+            end
+        end
+    end
+
+    def sort 
+        nu_hash = {}
+        roster.each do |x, y| 
+          nu_hash[x] = y.sort 
+        end 
+        nu_hash
+    end    
+end
